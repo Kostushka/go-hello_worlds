8cc9b6a91d911c311d73af7c8f2fb31516e34d04
user <nastya@debian-BULLSEYE-live-builder-AMD64>
2023-10-29 05:02:27 +0300

    up

--- chapter1/go/server1.go
+++ chapter1/go/server1.go
@@ -7,11 +7,12 @@ import (
 )
 
 func main() {
-
+	// связывает функцию обработчик с /, запускает сервер с портом 8000
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// путь отправить в качестве ответа
 	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
 }
---
+++ chapter1/go/server2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"log"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	// два обработчика двух урлов
+	http.HandleFunc("/", handler) // каждый обработчик запускается в отдельной go-подпрограмме
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// доступ к общей переменной должен быть обеспечен только в одном потоке, остальные ждут
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	// доступ к общей переменной должен быть обеспечен только в одном потоке, остальные ждут
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
